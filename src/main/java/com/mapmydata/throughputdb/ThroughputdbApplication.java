f16e7567175be5db3bc4247dd360274af285de93
daisie_local <daisieh@gmail.com>
2018-04-06 22:21:51 -0700

    spring initializr

---
+++ src/main/java/com/mapmydata/throughputdb/ThroughputdbApplication.java
@@ -0,0 +1,12 @@
+package com.mapmydata.throughputdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThroughputdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThroughputdbApplication.class, args);
+	}
+}
